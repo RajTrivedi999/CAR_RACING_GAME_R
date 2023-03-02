b9342d4f58bab060fbf167fb8cdefebfa6ebb7a5
Your Name <you@example.com>
2023-03-03 03:27:02 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var road=document.querySelector('.road')
 var score=document.querySelector('.score')
 var start=document.querySelector('.start')
-
+let myScore=0
 let myCarPosition = {
-    x:0,
-    y:0,
     speed:5
 }
 
@@ -19,43 +17,79 @@ function moveLine(){
     const lines=document.querySelectorAll('.line')
     
     lines.forEach(line =>{
-        var top=line.offsetTop
+        var top2=line.offsetTop
         const roadBox= road.getBoundingClientRect()
-        if(line.offsetTop>roadBox.bottom){
-            top=0
-            
+        if(line.offsetTop>roadBox.bottom-20){
+            top2=0
         }
-    line.style.top=top + myCarPosition.speed + 'px'
+    line.style.top=top2 + myCarPosition.speed + 'px'
     })
 }
 
+function moveEnemy(myCar){
+    const enemies=document.querySelectorAll('.enemy')
+    
+    enemies.forEach(e =>{
+        const enemyBox=e.getBoundingClientRect()
+        const myBox=myCar.getBoundingClientRect()
+        if(contact(myBox,enemyBox)){
+            
+        }
+        e.y+= myCarPosition.speed
+        e.style.top=e.y + 'px'
+        if(e.y > 820){
+            e.y=-350
+            e.style.top=e.y + 'px'
+            e.style.backgroundColor=enemyColor()
+            e.style.left=Math.floor(Math.random()* 512) + 'px'
+        }
+    })
+}
+
+function contact(myCar,enemyCar){
+    if(myCar.top < enemyCar.bottom && myCar.right > enemyCar.left && myCar.bottom > enemyCar.top && myCar.left < enemyCar.right){
+        myCarPosition.start=false
+        road.innerHTML=""
+        start.classList.remove('hide')
+        start.innerHTML="GAME OVER <br> YOUR SCORE IS: " + myScore + "<br> CLICK HERE TO START AGAIN"
+        myScore=0
+        myCarPosition.speed=5
+    }
+}
+
 function renderGame(milliseconds){
-    moveLine()
     const myCar=document.querySelector('.myCar')
     const box= road.getBoundingClientRect();
-    if(player.ArrowUp && myCarPosition.y > box.top){
-        myCarPosition.y -= myCarPosition.speed
-        console.log(myCarPosition.y)
+    score.innerHTML="SCORE : " + myScore
+    myScore++
+    if(myScore%100==0) myCarPosition.speed+=1
+    if(myCarPosition.start){
+        moveLine()
+        moveEnemy(myCar)
+        if(player.ArrowUp && myCarPosition.y > box.top){
+            myCarPosition.y -= myCarPosition.speed
+            console.log(myCarPosition.y)
+        }
+        if(player.ArrowDown && myCarPosition.y < box.bottom-120){
+            myCarPosition.y += myCarPosition.speed
+            console.log(myCarPosition.y)
+        }
+        if(player.ArrowRight && myCarPosition.x < box.width-92){
+            myCarPosition.x += myCarPosition.speed
+        }
+        if(player.ArrowLeft && myCarPosition.x > 4){
+            myCarPosition.x -= myCarPosition.speed
+        }
     }
-    if(player.ArrowDown && myCarPosition.y < box.bottom-120){
-        myCarPosition.y += myCarPosition.speed
-        console.log(myCarPosition.y)
-    }
-    if(player.ArrowRight && myCarPosition.x < box.right-88){
-        myCarPosition.x += myCarPosition.speed
-    }
-    if(player.ArrowLeft && myCarPosition.x > box.left+8){
-        myCarPosition.x -= myCarPosition.speed
-    }
-
     myCar.style.top= myCarPosition.y + 'px'
     myCar.style.left= myCarPosition.x + 'px'
-    console.log(myCarPosition.x + 'px');
+    
     window.requestAnimationFrame(renderGame)
 }
 
 function startgame(){
     start.classList.add('hide')
+    myCarPosition.start=true
     let top=0;
     for(var i=0;i<5;i++){
         var line=document.createElement('div')
@@ -75,8 +109,9 @@ function startgame(){
     for(var i=0;i<3;i++){
         let enemy=document.createElement('div')
         enemy.classList.add('enemy')
-        enemy.y=((i+1)*320)*(-1)
-        
+        enemy.y=((i+1)*400)*(-1)
+        //alert(enemy.y)
+        enemy.style.top=enemy.y + 'px'
         enemy.style.backgroundColor=enemyColor()
         enemy.style.left=Math.floor(Math.random() * 512) + 'px'
         road.appendChild(enemy)
